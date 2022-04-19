2582853599637aaa6c19d64fbafae52a2881caae
Yosuke Tanigawa <tanigawa@mit.edu>
2022-04-19 10:18:54 -0400

    minor bug fix

--- R/sparse.R
+++ R/sparse.R
@@ -6,45 +6,45 @@ defaultVariantFilter = function(snps_to_use_arg){
 
 
 #' @export
-snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter=defaultVariantFilter, GroupMap=NULL, family = NULL, 
-    covariates = NULL, sparse=TRUE, nlambda = 100, lambda.min.ratio = 1e-4, lambda = NULL, split.col = NULL, 
+snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter=defaultVariantFilter, GroupMap=NULL, family = NULL,
+    covariates = NULL, sparse=TRUE, nlambda = 100, lambda.min.ratio = 1e-4, lambda = NULL, split.col = NULL,
     p.factor = NULL, status.col = NULL, mem = NULL, configs = NULL) {
     time.start <- Sys.time()
     snpnetLogger("Start snpnet", log.time = time.start)
-    
+
     snpnetLogger("Preprocessing start..")
-    
+
     ### --- Read genotype IDs --- ###
     ids <- list()
     phe <- list()
     ids[["psam"]] <- readIDsFromPsam(paste0(genotype.pfile, ".psam"))
-    
+
     ### --- combine the specified configs with the default values --- ###
-    if (!is.null(lambda)) 
+    if (!is.null(lambda))
         nlambda <- length(lambda)
-    configs <- setupConfigs(configs, genotype.pfile, phenotype.file, phenotype, covariates, 
+    configs <- setupConfigs(configs, genotype.pfile, phenotype.file, phenotype, covariates,
         1, nlambda, split.col, p.factor, status.col, mem)
     ### --- Read phenotype file --- ###
-    phe[["master"]] <- readPheMaster(phenotype.file, ids[["psam"]], family, covariates, 
+    phe[["master"]] <- readPheMaster(phenotype.file, ids[["psam"]], family, covariates,
         phenotype, status.col, split.col, configs)
-    
+
     ### --- infer family and update the configs --- ###
-    if (is.null(family)) 
+    if (is.null(family))
         family <- inferFamily(phe[["master"]], phenotype, status.col)
     configs <- updateConfigsWithFamily(configs, family)
-    
+
     ### --- Process phenotypes --- ###
     if (family == "binomial") {
         # The input binary phenotype is coded as 2/1 (case/control) For glmnet, we map
         # this to 1/0 (case/control) The following expression will replace -9 (missing)
         # with -10, but the set of individuals with no-missing values are already
         # computed.
-        if (min(phe[["master"]][[phenotype]], na.rm = T) >= 1 && max(phe[["master"]][[phenotype]], 
+        if (min(phe[["master"]][[phenotype]], na.rm = T) >= 1 && max(phe[["master"]][[phenotype]],
             na.rm = T) <= 2) {
             phe[["master"]][[phenotype]] <- phe[["master"]][[phenotype]] - 1
         }
     }
-    
+
     ### --- Define the set of individual IDs for training (and validation) set(s) ---
     validation <- (!is.null(split.col))
     if (validation) {
@@ -56,7 +56,7 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
         splits <- c("train")
         ids[["train"]] <- phe[["master"]]$ID
     }
-    
+
     ### --- Prepare the feature matrix --- ###
     features <- list()
     for (s in splits) {
@@ -67,10 +67,10 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
         } else {
             features[[s]] <- NULL
         }
-        if (configs[["verbose"]]) 
+        if (configs[["verbose"]])
             snpnetLogger(sprintf("The number of individuals in %s set: %d", s, dim(phe[[s]])[1]))
     }
-    
+
     ### --- Prepare the response --- ###
     response <- list()
     status <- list()
@@ -82,22 +82,22 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
             response[[s]] <- survival::Surv(response[[s]], status[[s]])
         }
     }
-    
+
     ### --- Read genotypes --- ###
-    vars <- dplyr::mutate(dplyr::rename(data.table::fread(cmd = paste0(configs[["zstdcat.path"]], 
-        " ", paste0(genotype.pfile, ".pvar.zst"))), CHROM = "#CHROM"), VAR_ID = paste(ID, 
+    vars <- dplyr::mutate(dplyr::rename(data.table::fread(cmd = paste0(configs[["zstdcat.path"]],
+        " ", paste0(genotype.pfile, ".pvar.zst"))), CHROM = "#CHROM"), VAR_ID = paste(ID,
         ALT, sep = "_"))$VAR_ID
     pvar <- pgenlibr::NewPvar(paste0(genotype.pfile, ".pvar.zst"))
-    
+
     pgen <- list()
     samples_subset <- list()
     for (s in splits) {
         samples_subset[[s]] <- match(ids[[s]], ids[["psam"]])
-        pgen[[s]] <- pgenlibr::NewPgen(paste0(genotype.pfile, ".pgen"), pvar = pvar, 
+        pgen[[s]] <- pgenlibr::NewPgen(paste0(genotype.pfile, ".pgen"), pvar = pvar,
             sample_subset = samples_subset[[s]])
     }
     pgenlibr::ClosePvar(pvar)
-    
+
     snps_to_use <- computeSparseStats(genotype.pfile, phe[["train"]]$ID, configs = configs)
 
 
@@ -110,7 +110,7 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
     }
     snpnetLoggerTimeDiff("Preprocessing end.", time.start, indent = 1)
     snpnetLogger(paste("Number of variants to use is", nrow(snps_to_use), "Training data size is", nrow(phe[['train']])))
-    
+
     ### --- Fit a model using only the covariates
     offset <- list()
     if (family == "cox") {
@@ -123,38 +123,38 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
             offset[['val']] <-  as.matrix(phe[['val']] %>%  dplyr::select(all_of(covariates))) %*% glmmod$beta
         }
     } else {
-        glmmod <- stats::glm(stats::as.formula(paste(phenotype, " ~ ", paste(c(1, 
+        glmmod <- stats::glm(stats::as.formula(paste(phenotype, " ~ ", paste(c(1,
             covariates), collapse = " + "))), data = phe[["train"]], family = family)
     }
-    
+
     group_reg = !is.null(GroupMap)
     if(group_reg){
         proxObj <- pgenlibr::NewProxObj(nrow(snps_to_use), gene_cumu)
     } else {
         proxObj <- pgenlibr::NewLassoObj(nrow(snps_to_use))
     }
-    
-    
+
+
     gaussian_response_sd <- NULL
     responseObj <- NULL
     # Run linear prediction
     if(family != "cox"){
         for (s in splits) {
-            offset[[s]] <- stats::predict(glmmod, phe[[s]] %>% dplyr::select(all_of(covs)))
+            offset[[s]] <- stats::predict(glmmod, phe[[s]] %>% dplyr::select(all_of(covariates)))
         }
     }
 
     if (family == "gaussian") {
         gaussian_response_sd <- sd(glmmod$residuals)
-        responseObj <- pgenlibr::NewResponseObj(glmmod$residuals/gaussian_response_sd, 
+        responseObj <- pgenlibr::NewResponseObj(glmmod$residuals/gaussian_response_sd,
             "gaussian")
     } else if (family == "binomial") {
         responseObj <- pgenlibr::NewResponseObj(response[['train']], "binomial", offset[['train']])
     } else if (family == "cox") {
         responseObj <- getCoxResponseObj(response[['train']], offset[['train']])
     }
-    
-    
+
+
     time.load.matrix <- Sys.time()
     snpnetLogger("Start loading training genotype matrix")
     if(sparse){
@@ -170,7 +170,7 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
             warning("Loading sparse training matrix failed. This happens most likely because the reference allele is not the major allele, or some variants have large number of NAs. Use Dense instead.")
         }
     }
-    
+
     if(sparse){
         MULT_FUN <- pgenlibr::SparseMultv
     } else {
@@ -178,35 +178,35 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
         MULT_FUN <- pgenlibr::DenseMultv
     }
 
-    
+
     if(validation){
         Xval <- pgenlibr::NewDense(pgen[["val"]], snps_to_use$index, snps_to_use$stats_means)
     }
     snpnetLoggerTimeDiff("End loading genotype matrix", time.load.matrix, indent = 1)
-    
+
     if (is.null(lambda)) {
         if(group_reg){
             lambda.max <- pgenlibr::ComputeLambdaMax(Xtrain, responseObj, gene_cumu)
         } else {
             lambda.max <- pgenlibr::ComputeLambdaMax(Xtrain, responseObj, seq(0, nrow(snps_to_use)))
         }
-        full.lams <- exp(seq(from = log(lambda.max), to = log(lambda.max * lambda.min.ratio), 
+        full.lams <- exp(seq(from = log(lambda.max), to = log(lambda.max * lambda.min.ratio),
             length.out = nlambda))
     } else {
         full.lams <- lambda
     }
-    
+
     metric.train <- rep(NA, length(full.lams))
     metric.val <- rep(NA, length(full.lams))
     max.metric.val <- -1
-    
-    
+
+
     if (validation) {
         lambda_schedule <- round(c(0, 0.05*(1:20)) * length(full.lams))
     } else {
         lambda_schedule <- c(0, length(full.lams))
     }
-    
+
     beta = NULL
     early.stop = FALSE
 
@@ -228,10 +228,10 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
             pred.train[, j] <- MULT_FUN(Xtrain, result[, j])
         }
 
-        pred.train <- sweep(pred.train, 1,  offset[["train"]], "+")  
-        metric.train[lambda_start_ind:lambda_end_ind] <- computeMetric(pred.train, response[["train"]], 
+        pred.train <- sweep(pred.train, 1,  offset[["train"]], "+")
+        metric.train[lambda_start_ind:lambda_end_ind] <- computeMetric(pred.train, response[["train"]],
             configs[["metric"]])
-        
+
         if (validation) {
             snpnetLogger("Evaluating validation metric")
             pred.val = matrix(nrow=length(response[["val"]]), ncol=ncol(result))
@@ -239,10 +239,10 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
                 pred.val[,j] <- pgenlibr::DenseMultv(Xval, result[, j])
             }
 
-            pred.val <- sweep(pred.val, 1,  offset[["val"]], "+")  
-            metric.val[lambda_start_ind:lambda_end_ind] <- computeMetric(pred.val, response[["val"]], 
+            pred.val <- sweep(pred.val, 1,  offset[["val"]], "+")
+            metric.val[lambda_start_ind:lambda_end_ind] <- computeMetric(pred.val, response[["val"]],
                 configs[["metric"]])
-            
+
             print(metric.train[1:lambda_end_ind])
             print(metric.val[1:lambda_end_ind])
 
@@ -250,7 +250,7 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
                 snpnetLoggerTimeDiff("Early stopping condition reached", time.start, indent = 1)
                 break
             }
-            
+
             max.metric.val <- max(metric.val, na.rm = T)
             # Update early.stop after checking to go one iteration further in the path
             early.stop = metric.val[lambda_end_ind] < max.metric.val
@@ -258,26 +258,26 @@ snpnet2Base <- function(genotype.pfile, phenotype.file, phenotype, VariantFilter
         }
         snpnetLoggerTimeDiff(paste("Iteration", i, "done"), time.iter, indent = 1)
     }
-    
+
     rownames(beta) <- snps_to_use$original_ID
     metric.train = metric.train[1:lambda_end_ind]
     metric.val = metric.val[1:lambda_end_ind]
-    
+
     return(list(beta=beta, covs_fit=glmmod, snps_used=snps_to_use, metric.train=metric.train, metric.val=metric.val, lambda=full.lams[1:lambda_end_ind]))
-    
+
 }
 
 
 #' @export
-sparse_snpnet <- function(genotype.pfile, phenotype.file, phenotype, group_map, family = NULL, 
-    covariates = NULL, sparse=TRUE, nlambda = 100, lambda.min.ratio = 1e-4, lambda = NULL, split.col = NULL, 
+sparse_snpnet <- function(genotype.pfile, phenotype.file, phenotype, group_map, family = NULL,
+    covariates = NULL, sparse=TRUE, nlambda = 100, lambda.min.ratio = 1e-4, lambda = NULL, split.col = NULL,
     p.factor = NULL, status.col = NULL, mem = NULL, configs = NULL, variant_filter=NULL) {
 
         ### The mapping file must have these columns
     gene_map <- data.table::fread(group_map, select = c("#CHROM", "POS", "SYMBOL", "Csq"), colClasses=c(POS="integer"))
     names(gene_map)[1] <- "CHROM"
     names(gene_map)[3] <- "gene_symbol"
-    
+
     gene_map$CHROM[gene_map$CHROM == "X"] <- "23"
     gene_map$CHROM[gene_map$CHROM == "Y"] <- "24"
     gene_map$CHROM <- as.integer(gene_map$CHROM)
@@ -285,12 +285,12 @@ sparse_snpnet <- function(genotype.pfile, phenotype.file, phenotype, group_map, 
     if (max(unique_chrom) != length(unique_chrom)) {
         stop("The chromosome must be stored in the order 1,2,3,..., X, Y")
     }
-    
+
     cumu_chrom <- integer(length(unique_chrom) + 1)
     for (i in (1:length(unique_chrom))) {
         ind <- which(gene_map$CHROM == i)
         cumu_chrom[i + 1] <- cumu_chrom[i] + length(ind)
-        
+
         requirement <- all(diff(gene_map$POS[ind]) >= 0) && (ind[1] == cumu_chrom[i] + 1)
         if (!requirement) {
             stop("The gene mapping must have all SNPs on the same chromosome stored contiguously, and the position of the SNPs within the same chromosome must be non-decreasing")
@@ -312,28 +312,28 @@ sparse_snpnet <- function(genotype.pfile, phenotype.file, phenotype, group_map, 
         snps_to_use_arg$CHROM[snps_to_use_arg$CHROM == "Y"] <- "24"
         snps_to_use_arg <- snps_to_use_arg[, CHROM:=as.integer(CHROM)]
 
-        snps_gene_ind <- pgenlibr::match_sorted_snp(snps_to_use_arg$CHROM, snps_to_use_arg$POS, 
+        snps_gene_ind <- pgenlibr::match_sorted_snp(snps_to_use_arg$CHROM, snps_to_use_arg$POS,
         gene_map$POS, cumu_chrom)
-    
+
         # Just for testing,
         if(!all(snps_to_use_arg$POS == gene_map$POS[snps_gene_ind])){
             stop("something is wrong, most likely the group mapping file does not contain all the variant in the pgen file.")
         }
         snps_to_use_arg$gene <- gene_map$gene_symbol[snps_gene_ind]
         snps_to_use_arg$Csq <- gene_map$Csq[snps_gene_ind]
-        
+
         # Only use autosome
-        snps_to_use_arg <- snps_to_use_arg %>% dplyr::filter(CHROM < 23) %>% dplyr::filter(gene != 
+        snps_to_use_arg <- snps_to_use_arg %>% dplyr::filter(CHROM < 23) %>% dplyr::filter(gene !=
             "" & (Csq %in% c("ptv", "pav")))
         # snps_to_use = snps_to_use %>% filter(Csq %in% c('ptv', 'pav'))
-        
+
         genes_with_comma <- grepl(",", snps_to_use_arg$gene, fixed = TRUE)
         if (any(genes_with_comma)) {
             warning("Some SNPs are mapped to multiple genes. The first gene symbols will be used to group these SNPs")
-            snps_to_use_arg$gene[genes_with_comma] <- sapply(strsplit(snps_to_use_arg$gene[genes_with_comma], 
+            snps_to_use_arg$gene[genes_with_comma] <- sapply(strsplit(snps_to_use_arg$gene[genes_with_comma],
                 ","), "[", 1)
         }
-        
+
         unique_genes <- unique(snps_to_use_arg$gene)
         snps_to_use_arg$gene_order <- match(snps_to_use_arg$gene, unique_genes)
         snps_to_use_arg <- snps_to_use_arg %>% dplyr::arrange(gene_order)
@@ -347,8 +347,8 @@ sparse_snpnet <- function(genotype.pfile, phenotype.file, phenotype, group_map, 
     }
 
 
-    snpnet2Base(genotype.pfile, phenotype.file, phenotype, VariantFilter, GroupMap, family, 
-    covariates, sparse, nlambda, lambda.min.ratio, lambda, split.col, 
+    snpnet2Base(genotype.pfile, phenotype.file, phenotype, VariantFilter, GroupMap, family,
+    covariates, sparse, nlambda, lambda.min.ratio, lambda, split.col,
     p.factor, status.col, mem, configs)
 
 }
